0e5a050fff06cc21ddc7cac3292fbea55bd1b91b
DongMinKwon <wt0630@naver.com>
2023-01-11 15:50:38 +0900

    docker ganache connection

--- server/src/blockchain/index.js
+++ server/src/blockchain/index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const Web3 = require('web3');
-const web3J = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+const web3J = new Web3(
+  new Web3.providers.HttpProvider('http://host.docker.internal:7545')
+);
 
 const Web3Contract = require('web3-eth-contract');
 
 // ABI와 contract address 가져오기
 const tokenABI = require('./ABI/MyToken');
-const {TOKEN_CA} = process.env;
+const { TOKEN_CA } = process.env;
 
-Web3Contract.setProvider('http://127.0.0.1:7545');
+Web3Contract.setProvider('http://host.docker.internal:7545');
 const tokenContract = new Web3Contract(tokenABI, TOKEN_CA);
 
-module.exports={
+module.exports = {
   web3J,
-  tokenContract
+  tokenContract,
 };
